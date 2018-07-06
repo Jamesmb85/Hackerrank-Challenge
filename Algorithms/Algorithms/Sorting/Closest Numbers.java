53a58cf579d01e1d15c178f67db30f5ac55e38b1
James Brumfield <35109113+Jamesmb85@users.noreply.github.com>
2018-07-06 16:51:29 -0400

    Update Closest Numbers.java

--- Algorithms/Algorithms/Sorting/Closest Numbers.java
+++ Algorithms/Algorithms/Sorting/Closest Numbers.java
@@ -16,49 +16,49 @@ public class Solution {
         //use ArrayList since we don't know the size of array that meet the smallest_difference criteria 
         ArrayList<Integer> difference = new ArrayList<Integer>();
         
-		/*   let's assume the the smallest absolute difference is Integer.MAX_VALUE;
-		We do this because we want  an initial value to this variable
-		Integer.MAX_VALUE = 2147483647 */
+	/*   let's assume the the smallest absolute difference is Integer.MAX_VALUE;
+	We do this because we want  an initial value to this variable
+	Integer.MAX_VALUE = 2147483647 */
         int smallest_difference = Integer.MAX_VALUE;
         
-		/* let's loop through the array and check the difference between smallest_difference and the other elements. 
-		After loop will will know the smallest difference between all elements */
+	/* let's loop through the array and check the difference between smallest_difference and the other elements. 
+	After loop will will know the smallest difference between all elements */
         for(int i = 1; i < arr.length; i++){
             if(arr[i]-arr[i-1] < smallest_difference){
-				/* remove the elements from the ArrayList from the previous iteration. For example, if the smallest
-				difference is at the last 2 elements in the array, then the array is filled with entries when the true
-				smallest difference isn't discovered yet. */
+		/* remove the elements from the ArrayList from the previous iteration. For example, if the smallest
+		difference is at the last 2 elements in the array, then the array is filled with entries when the true
+		smallest difference isn't discovered yet. */
                 difference.clear();
                 
-				//if the difference is less than smallest_difference, then change value of smallest_difference
+		//if the difference is less than smallest_difference, then change value of smallest_difference
                 smallest_difference = arr[i]-arr[i-1];
                 
-				//place elements in the ArrayList
+		//place elements in the ArrayList
                 difference.add(arr[i]);
                 difference.add(arr[i-1]);
-            }
-			//Need to consider the case when one element is 0 and the order is the Maximum int value
-			else if(arr[i]-arr[i-1] == smallest_difference){
-				//place elements in the ArrayList
-                difference.add(arr[i]);
-                difference.add(arr[i-1]);
-			}
+            	}
+	//Need to consider the case when one element is 0 and the order is the Maximum int value
+		else if(arr[i]-arr[i-1] == smallest_difference){
+		//place elements in the ArrayList
+		difference.add(arr[i]);
+		 difference.add(arr[i-1]);
+		}
         }
         
         //sort ArrayList in ascending order
-		Collections.sort(difference);
+	Collections.sort(difference);
         
         
-		//need to place elements from ArrayList into an Array 
+	//need to place elements from ArrayList into an Array 
         //create array the same size as difference  
         int[] newArray = new int[difference.size()];
 
 		
-		//fill in newArray
-		for(int j = 0; j < newArray.length; j++){
-			//use get method to access value at index in the ArrayList
-			newArray[j] = difference.get(j);
-		}
+	//fill in newArray
+	for(int j = 0; j < newArray.length; j++){
+		//use get method to access value at index in the ArrayList
+		newArray[j] = difference.get(j);
+	}
 
         
         //return the array
