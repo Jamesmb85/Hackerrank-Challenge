af8c6f6ddee1c60962023b5c397725f487d5fd9c
James Brumfield <35109113+Jamesmb85@users.noreply.github.com>
2018-08-03 12:38:40 -0400

    Add files via upload

---
+++ Java/Introduction/Java Int to String.java
@@ -0,0 +1,49 @@
+mport java.util.*;
+import java.security.*;
+public class Solution {
+ public static void main(String[] args) {
+
+  DoNotTerminate.forbidExit();
+
+  try {
+   Scanner in = new Scanner(System.in);
+   int n = in .nextInt();
+   in.close();
+    //Write your code here
+    
+	/*Ok, so looks like we need to convert n to a string
+      The Integer class has a static method that returns a String object representing the specified int parameter. 
+	  Using this is an efficient solution. */
+    String s = Integer.toString(n); //this is equivalent to String s = "n";
+	
+	 if (n == Integer.parseInt(s)) {
+    System.out.println("Good job");
+   } else {
+    System.out.println("Wrong answer.");
+   }
+  } catch (DoNotTerminate.ExitTrappedException e) {
+   System.out.println("Unsuccessful Termination!!");
+  }
+ }
+}
+
+//The following class will prevent you from terminating the code using exit(0)!
+class DoNotTerminate {
+
+ public static class ExitTrappedException extends SecurityException {
+
+  private static final long serialVersionUID = 1;
+ }
+
+ public static void forbidExit() {
+  final SecurityManager securityManager = new SecurityManager() {
+   @Override
+   public void checkPermission(Permission permission) {
+    if (permission.getName().contains("exitVM")) {
+     throw new ExitTrappedException();
+    }
+   }
+  };
+  System.setSecurityManager(securityManager);
+ }
+}
