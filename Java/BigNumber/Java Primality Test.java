c0093074806641edb2be98cfbc2fb75b516eb457
James Brumfield <35109113+Jamesmb85@users.noreply.github.com>
2018-08-06 16:29:35 -0400

    Update Java Primality Test.java

--- Java/BigNumber/Java Primality Test.java
+++ Java/BigNumber/Java Primality Test.java
@@ -13,15 +13,15 @@ public class Solution {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-		// To create an object of Scanner class, we usually pass the predefined object System.in, which represents the standard input stream.
+	// To create an object of Scanner class, we usually pass the predefined object System.in, which represents the standard input stream.
         Scanner n = new Scanner(System.in);
         
-        //assign value to BigInterger instance
-		//next() function returns the next token/word in the input as a string 
+	//assign value to BigInterger instance
+	//next() function returns the next token/word in the input as a string 
         BigInteger b1 = new BigInteger(n.next());
         
-		/* isProbablePrime(int certainty): A method in BigInteger class to check if a given number is prime. 
-		For certainty = 1, it return true if BigInteger is prime and false if BigInteger is composite.*/
+	/* isProbablePrime(int certainty): A method in BigInteger class to check if a given number is prime. 
+	For certainty = 1, it return true if BigInteger is prime and false if BigInteger is composite.*/
         if(b1.isProbablePrime(1)){
             System.out.println("prime");
         }
