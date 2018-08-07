dd84e6e577a0a030b542f9f491b812443eeceeed
James Brumfield <35109113+Jamesmb85@users.noreply.github.com>
2018-08-06 20:48:07 -0400

    Update Java BigInteger.java

--- Java/BigNumber/Java BigInteger.java
+++ Java/BigNumber/Java BigInteger.java
@@ -9,17 +9,17 @@ public class Solution {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
   		
-		// To create an object of Scanner class, we usually pass the predefined object System.in, which represents the standard input stream.
+	// To create an object of Scanner class, we usually pass the predefined object System.in, which represents the standard input stream.
         Scanner n = new Scanner(System.in);
 		
-		//assign value to BigInterger instance
-		//next() function returns the next token/word in the input as a string
+	//assign value to BigInterger instance
+	//next() function returns the next token/word in the input as a string
         BigInteger b1 = new BigInteger(n.next());
         BigInteger b2 = new BigInteger(n.next());
         
-		//The java.math.BigInteger.add(BigInteger val) returns a BigInteger object whose value is (this + val).
+	//The java.math.BigInteger.add(BigInteger val) returns a BigInteger object whose value is (this + val).
         System.out.println(b1.add(b2));
-		//The java.math.BigInteger.multiply(BigInteger val) returns a BigInteger whose value is (this * val).
+	//The java.math.BigInteger.multiply(BigInteger val) returns a BigInteger whose value is (this * val).
         System.out.println(b1.multiply(b2));
     }
 }
