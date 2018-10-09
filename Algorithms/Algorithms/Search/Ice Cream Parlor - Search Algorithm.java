00f7fc47b105398a1e640d34eb6a947505f6e010
James Brumfield <35109113+Jamesmb85@users.noreply.github.com>
2018-10-09 14:17:57 -0400

    Add files via upload

---
+++ Algorithms/Algorithms/Search/Ice Cream Parlor - Search Algorithm.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the icecreamParlor function below.
+    static int[] icecreamParlor(int m, int[] arr) {
+        
+        //array we are going to return
+        int[] temp = new int[2];
+        
+        
+        //need a nested loop to check each value with every other value in the array
+        //outer loop starts at index 1 since this is a 1-based index array
+        for(int i = 0; i < arr.length; i++){
+            //inner loop starts at the 1 index greater than what the outer loop is
+            for(int j = i+1; j < arr.length; j++){
+                //test to see if values at indexes equal amount fo money they have
+                if(arr[i] + arr[j] == m){
+                    //places indexes in temp array
+                    temp[0] = i+1;
+                    temp[1] = j+1;
+                    return temp;
+                }
+                
+            }
+        }
+        
+        return temp;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int m = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] arr = new int[n];
+
+            String[] arrItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int arrItem = Integer.parseInt(arrItems[i]);
+                arr[i] = arrItem;
+            }
+
+            int[] result = icecreamParlor(m, arr);
+
+            for (int i = 0; i < result.length; i++) {
+                bufferedWriter.write(String.valueOf(result[i]));
+
+                if (i != result.length - 1) {
+                    bufferedWriter.write(" ");
+                }
+            }
+
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
