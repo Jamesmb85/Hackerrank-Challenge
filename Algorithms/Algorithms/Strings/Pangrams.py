ff35b82af38ecc38253d0690d81cede4d710edda
James Brumfield <35109113+Jamesmb85@users.noreply.github.com>
2021-07-19 22:08:40 -0400

    Add files via upload

---
+++ Algorithms/Algorithms/Strings/Pangrams.py
@@ -0,0 +1,104 @@
+"""
+A pangram is a string that contains every letter of the alphabet. Given a sentence determine whether it is a pangram in the English alphabet. Ignore case. Return either pangram or not pangram as appropriate.
+
+Example
+
+The string contains all letters in the English alphabet, so return pangram.
+
+Function Description
+
+Complete the function pangrams in the editor below. It should return the string pangram if the input string is a pangram. Otherwise, it should return not pangram.
+
+pangrams has the following parameter(s):
+
+    string s: a string to test
+
+Returns
+
+    string: either pangram or not pangram
+
+Input Format
+
+A single line with string
+
+.
+
+Constraints
+
+
+Each character of ,
+
+Sample Input
+
+Sample Input 0
+
+We promptly judged antique ivory buckles for the next prize
+
+Sample Output 0
+
+pangram
+
+Sample Explanation 0
+
+All of the letters of the alphabet are present in the string.
+
+Sample Input 1
+
+We promptly judged antique ivory buckles for the prize
+
+Sample Output 1
+
+not pangram
+
+Sample Explanation 0
+
+The string lacks an x.
+
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    # Write your code here
+
+    #let's create a list of the alphabet
+    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+    #let's work with all lower case letters and remove the whitespace
+    working_string = s.lower().strip()
+
+    #let's take the working_string and iterate through it and see if that letter in the the list. if it is, remove it
+    for letter in working_string:
+        if letter in alphabet:
+            #remove the letter from the list
+            alphabet.remove(letter)
+
+    #if all of the letters appear in the string, then the alphabet should be length 0
+    if len(alphabet) == 0:
+        return 'pangram'
+    else:
+        return 'not pangram'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
